968469a3d6b56af4b743b4f39fbfc825f224ea5d
Thang <thlal@amazon.com>
2019-09-12 16:06:29 -0400

    inicial commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,27 +2,34 @@
 
 
 let teamOneShotCountElement = document.querySelector("#teamone-numshots")
-let teamOneGoalCountElement =  document.querySelector("#teamone-numhits")
-let teamOneShootButtonElement =  document.querySelector("#teamone-shoot")
+let teamOneGoalCountElement = document.querySelector("#teamone-numhits")
+let teamOneShootButtonElement = document.querySelector("#teamone-shoot")
+let shoot1sound = new Audio("assets/audio/goalsound.mp3");
+let shoot2sound = new Audio("assets/audio/boo.mp3");
+
+
 
 let teamOneShotCount = 0;
 let teamOneGoalCount = 0;
 
-teamOneShootButtonElement.addEventListener("click", function(){
+teamOneShootButtonElement.addEventListener("click", function () {
     teamOneShotCount = teamOneShotCount + 1
     teamOneShotCountElement.innerHTML = teamOneShotCount
 
     //if shot was successful, increment goalcount by one
     //after incrementing goalcount, set goalcountelement.innerHTML
-    //equal to gaolcount
+    //equal to gaolcounta
+
+    shoot1sound.play();
 
 
-    if(Math.random() < .25){
+    if (Math.random() < .25) {
 
-    teamOneGoalCount = teamOneGoalCount +1
-    teamOneGoalCountElement.innerHTML = teamOneGoalCount
+        teamOneGoalCount = teamOneGoalCount + 1
+        teamOneGoalCountElement.innerHTML = teamOneGoalCount
 
     }
+    
 
 })
 
@@ -30,20 +37,23 @@ teamOneShootButtonElement.addEventListener("click", function(){
 let teamTwoShotCountElement = document.querySelector("#teamtwo-numshots")
 let teamTwoGoalCountElement = document.querySelector("#teamtwo-numhits")
 let teamTwoShootButtonElement = document.querySelector("#teamtwo-shoot")
-    
+
 let teamTwoShotCount = 0;
 let teamTwoGoalCount = 0;
-    
-teamTwoShootButtonElement.addEventListener("click", function(){
-        teamTwoShotCount = teamTwoShotCount + 1
-        teamTwoShotCountElement.innerHTML = teamTwoShotCount
 
-        if(Math.random() < .25){
+teamTwoShootButtonElement.addEventListener("click", function () {
+    teamTwoShotCount = teamTwoShotCount + 1
+    teamTwoShotCountElement.innerHTML = teamTwoShotCount
+    shoot2sound.play();
 
-        teamTwoGoalCount = teamTwoGoalCount +1
+
+
+    if (Math.random() < .25) {
+
+        teamTwoGoalCount = teamTwoGoalCount + 1
         teamTwoGoalCountElement.innerHTML = teamTwoGoalCount
-        
-     }  
+
+    }
 
 })
 
@@ -52,8 +62,8 @@ let NumberOfResetElement = document.querySelector("#num-resets")
 
 let resetButtonCount = 0;
 
-resetButtonElement.addEventListener("click", function(){
-    resetButtonCount = resetButtonCount +1
+resetButtonElement.addEventListener("click", function () {
+    resetButtonCount = resetButtonCount + 1
     NumberOfResetElement.innerHTML = resetButtonCount
     teamTwoGoalCountElement.innerHTML = 0;
     teamOneGoalCountElement.innerHTML = 0;
